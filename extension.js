b288dadd1920aecb1a8bf00b81a8762d679d3f03
robert-e-frasier <robert.e.frasier@gmail.com>
2025-05-06 11:45:24 -0700

    send updated files

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const { exec } = require('child_process');
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,12 +19,26 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('kivy-editor.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Kivy Editor!');
+	const disposable = vscode.commands.registerCommand('kivy-editor.start', function () {
+		exec('py -m pip show kivy', (error, stdout, stderr) => {
+			if (error || !stdout.includes('Name: Kivy')) {
+				vscode.window.showWarningMessage(
+					'Kivy is not installed. Click here to install it.',
+					'Install Kivy'
+				).then(selection => {
+					if (selection === 'Install Kivy') {
+						const terminal = vscode.window.createTerminal('Kivy Installer');
+						terminal.show();
+						terminal.sendText('py -m pip install kivy[full]');
+					}
+				});
+			} else {
+				vscode.window.showInformationMessage('Kivy is installed!');
+				// This is where we’ll launch the editor view in Phase 3
+			}
+		});
 	});
+	
 
 	context.subscriptions.push(disposable);
 }
